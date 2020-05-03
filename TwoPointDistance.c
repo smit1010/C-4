34db11950b8b7c2f05a6866c08aea2358272e6d8
Taehee <44861205+wonderfulhuman@users.noreply.github.com>
2020-05-03 16:45:22 +0900

    Create TwoPointDistance.c

---
+++ TwoPointDistance.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+
+struct point{
+    int x;
+    int y;
+};
+int main(){
+    struct point pos1, pos2;
+    double distance;
+    fputs("Input point1 pos: ", stdout);
+    scanf("%d %d", &pos1.x, &pos1.y);
+
+    fputs("Input point2 pos: ", stdout);
+    scanf("%d %d", &pos2.x, &pos2.y);
+
+    distance = sqrt((double)((pos1.x-pos2.x)*(pos1.x-pos2.x) + (pos1.y-pos2.y)*(pos1.y-pos2.y)));
+
+    printf("두 점의 거리는 %g 입니다.", distance);
+    return 0;
+}
